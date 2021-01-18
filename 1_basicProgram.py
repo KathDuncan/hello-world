b1a4eb7e7dd81b9d68b74460e3da1e21c3295903
KathDuncan <77627403+KathDuncan@users.noreply.github.com>
2021-01-18 11:24:15 -0400

    Add files via upload

---
+++ 1_basicProgram.py
@@ -0,0 +1,29 @@
+""" /********************************************
+
+This is the first part of the training course of Python
+Instructor: Ricardo Bautista Quintero
+Date: 18 Jan 2021
+NSCC
+Ivany Campus
+/************************************************"""
+"""
+
+We will learn about the following topics:
+    1.) Types of Numbers in Python
+    2.) Basic Arithmetic
+    3.) Differences between classic division and floor division
+    4.) Object Assignment in Python
+    
+"""
+
+def program_1():
+    a = 2
+    f = 2e4
+    
+    print(a)
+    print("is an integer")
+    print(f)
+    print("is a floating point number")
+    
+if__name__=='main':
+    program_1()
